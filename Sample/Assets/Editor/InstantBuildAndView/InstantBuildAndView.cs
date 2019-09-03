3e6bb2bfad349e27fcd684fadc30c9eee809da2b
常深 健太 <tsunefuka-k@ad.klab.org>
2019-09-03 20:34:36 +0900

    first commit

---
+++ Sample/Assets/Editor/InstantBuildAndView/InstantBuildAndView.cs
@@ -0,0 +1,215 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using System.Text;
+
+
+namespace InstantBuild
+{
+
+    public class InstantBuildAndView : EditorWindow
+    {
+        static InstantBuildAndView currentWindow;
+
+        static void BuildAssetBundles(BuildTarget buildTarget)
+        {
+
+            // １個だけ選択中許可
+            if (Selection.assetGUIDs != null && Selection.assetGUIDs.Length == 1)
+            {
+                // GUIDからAssetPathを得る
+                var assetPath = AssetDatabase.GUIDToAssetPath(Selection.assetGUIDs[0]);
+                Debug.Log("インスタントビルドを開始します。 " + assetPath);
+                // インスタントビルド開始
+                StartInstantBuild(assetPath, buildTarget);
+            }
+            else
+            {
+                Debug.LogWarning("ビルドしたい対象を1つ選んで実行してください。");
+            }
+        }
+
+        // Projectビューの右クリックメニューに登録
+        [MenuItem("Assets/即席ビルド/Android")]
+        static void AndroidBuildAssetBundles()
+        {
+            BuildAssetBundles(BuildTarget.Android);
+        }
+
+        // Projectビューの右クリックメニューに登録
+        [MenuItem("Assets/即席ビルド/iOS")]
+        static void iOSBuildAssetBundles()
+        {
+            BuildAssetBundles(BuildTarget.iOS);
+        }
+
+        /// <summary>
+        /// インスタントビルドの開始
+        /// </summary>
+        /// <param name="targetAssetPath"></param>
+        /// <param name="buildTarget"></param>
+        private static void StartInstantBuild(string targetAssetPath, BuildTarget buildTarget)
+        {
+            var dirPath = @"Temp/_InstantBuild/Android";
+
+            // 検索用時間文字列
+            var time = DateTime.Now.ToString("yyyyMMddHHmmss");
+            // 開始タグ
+            var startTag = "SINGLE BUILD START " + time;
+            // 終了タグ
+            var finishTag = "SINGLE BUILD FINISH " + time;
+            // 開始タグを出す
+            Debug.Log(startTag);
+
+            // AssetBundle一つを作成する場合
+            AssetBundleBuild[] buildMap = new AssetBundleBuild[1];
+
+            buildMap[0].assetBundleName = "InstantAssetBundle.unity3d";            // ビルドするだけなので名前は固定
+            buildMap[0].assetBundleVariant = "";                                // とりあえずバリアントいらない
+            buildMap[0].assetNames = new string[1] { targetAssetPath };         // 選択中のtargetのパスを渡す
+
+            // 存在しない場合はディレクトリ作る
+            if (!Directory.Exists(dirPath))
+            {
+                var dirInfo = Directory.CreateDirectory(dirPath);
+                if (dirInfo == null) return;
+            }
+
+            // 強制リビルド（ForceRebuildAssetBundle）
+            var manifest = BuildPipeline.BuildAssetBundles(dirPath, buildMap, BuildAssetBundleOptions.ForceRebuildAssetBundle, buildTarget);
+            if (manifest == null)
+            {
+                throw new Exception("アセットバンドルの生成に失敗しました");
+            }
+
+			// 終了タグを出す
+            Debug.Log(finishTag);
+
+			#if UNITY_EDITOR_WIN
+            // C:\Users\username\AppData\Local\Unity\Editor\Editor.log
+            // Windowsの場合、AppData/Localのパスを取得
+            var appLocalPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            // UnityEditorログのパス
+            var unityEditorLogPath = "\\Unity\\Editor\\Editor.log";
+			#else
+			// ~/Library/Logs/Unity/Editor.log
+			var appLocalPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			// UnityEditorログのパス
+			var unityEditorLogPath = "/Library/Logs/Unity/Editor.log";
+			#endif
+
+            // リードオンリーでストリームを開く
+            FileStream fs = new FileStream(appLocalPath + unityEditorLogPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            StreamReader sr = new StreamReader(fs);
+            //
+            var tmpBuff = new StringBuilder();
+            //
+            bool isTargetRange = false;
+            int tagCount = 0;
+
+            // Editor.logから読むために泥臭いことして読み込む
+            for (;;)
+            {
+                // 1行ずつ読み込み
+                var line = sr.ReadLine();
+                // 終わったら終了
+                if (line == null) break;
+                // 開始行
+                if (line.IndexOf(startTag) >= 0) isTargetRange = true;
+                // 閉じ行
+                if (line.IndexOf(finishTag) >= 0) isTargetRange = false;
+
+                // 今回の出力範囲
+                if (isTargetRange)
+                {
+                    // 出現回数をカウント
+                    if (line.IndexOf("***Player size statistics***") >= 0) tagCount++;
+                    // １回目～２回目の間をバッファに出力
+                    if (tagCount == 1)
+                    {
+                        tmpBuff.AppendLine(line);
+                    }
+                }
+            }
+
+            // ウィンドウなかったら
+            if (!currentWindow)
+            {
+                // ウィンドウをつくる
+                currentWindow = GetWindow(typeof(InstantBuildAndView)) as InstantBuildAndView;
+                Texture icon = AssetDatabase.LoadAssetAtPath<Texture>("Assets/Editor/InstantBuildAndView/Icon/icon_ibv.png");
+                currentWindow.titleContent = new GUIContent("BuildInfo", icon);
+            }
+            // ウィンドウ表示
+            currentWindow.Show();
+            // ウィンドウのバッファにログを与える
+            currentWindow.SetViewLog(tmpBuff.ToString(), targetAssetPath);
+            // 
+
+
+        }
+
+        /// <summary>
+        /// ログ情報渡し
+        /// </summary>
+        /// <param name="log"></param>
+        /// <param name="targetAssetPath"></param>
+        public void SetViewLog(string log, string targetAssetPath)
+        {
+            // アセット名
+            assetPathView = targetAssetPath;
+
+            windowViewLog = log;
+            // 詳細情報の構成
+            info = new InstantBuildInfo(log);
+        }
+
+        bool isGraph = false;
+        InstantBuildInfo info;
+        string assetPathView = "";
+        string windowViewLog = "";
+        Vector2 scrollPosition;
+        void OnGUI()
+        {
+            // 情報が無い場合は描かない
+            if (info == null) return;
+
+            // ヘッダの色帯
+            Handles.DrawSolidRectangleWithOutline(new Rect(0, 0, EditorGUIUtility.currentViewWidth, 34), new Color(0.3f, 0.33f, 0.33f), Color.clear);
+
+            GUIStyle guiStyle = new GUIStyle();
+            guiStyle.fontSize = 12;
+            guiStyle.normal.textColor = Color.white;
+            // ファイル名とサイズの表示
+            GUILayout.Label(" " + assetPathView, guiStyle);
+            guiStyle.fontSize = 18;
+            GUILayout.Label(" total size : " + info.SizeSumKB, guiStyle);
+            //
+            var startY = 40f;
+
+            // グラフ表示指定の場合
+            if (isGraph)
+            {
+                // 詳細情報GUI
+                info.DrawOnGUI();
+                startY = 220f;
+            }
+
+            // スクロールエリアを作る
+            GUILayout.BeginArea(new Rect(0, startY, Screen.width, Screen.height));
+            scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(Screen.width), GUILayout.Height(Screen.height - startY));
+
+            // グラフ表示のON/OFF
+            isGraph = GUILayout.Toggle(isGraph, "詳細グラフの表示");
+
+            // クリップボード転送ボタン
+            if (GUILayout.Button("クリップボードに転送")) { EditorGUIUtility.systemCopyBuffer = windowViewLog; }
+            // テキスト表示
+            EditorGUILayout.TextArea(windowViewLog);
+
+            GUILayout.EndScrollView();
+            GUILayout.EndArea();
+        }
+    }
+}
---
+++ Sample/Assets/Editor/InstantBuildAndView/InstantBuildInfo.cs
@@ -0,0 +1,212 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace InstantBuild
+{
+    public class InstantBuildInfo
+    {
+        // キーバリューリスト
+        Dictionary<string, float> datas = new Dictionary<string, float>();
+
+        // 合計容量を返す
+        public float SizeSum
+        {
+            get
+            {
+                return datas.Sum(x => x.Value);
+            }
+        }
+
+        public string SizeSumKB
+        {
+            get
+            {
+                return SizeSum.ToString("0.0") + " kb";
+            }
+        }
+
+        /// <summary>
+        /// ログ文字列から情報を起こす
+        /// </summary>
+        /// <param name="log"></param>
+        public InstantBuildInfo(string log)
+        {
+            // データ初期化
+            datas = new Dictionary<string, float>();
+
+            // ピックアップしたい要素
+            var picks = new string[] { "Textures", "Meshes", "Animations", "Sounds", "Shaders", "Other Assets", "Levels", "Scripts", "Included DLLs", "File headers", "Complete size" };
+
+            // 改行で分割して配列へ
+            string[] lines = log.Split('\n');
+            // 行数ぶん実行
+            foreach (var line in lines)
+            {
+                // ピックアップ要素対象
+                foreach (var pick in picks)
+                {
+                    // 前方一致で必要なキーワード検索
+                    if (line.StartsWith(pick))
+                    {
+                        // スペースを含んだピックアップ要素があるので消しておく
+                        var tmp = line.Replace(pick, "");
+                        // 空白で分解
+                        var splits = tmp.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        // 有効な配列が取れたら
+                        if (splits.Length >= 1)
+                        {
+                            var key = pick;
+                            var val = float.Parse(splits[0]);
+                            if (splits[1].Contains("mb")) val *= 1000f; // MB単位のデータがあった場合は値を1000倍
+                            datas.Add(key, val);
+                        }
+                    }
+                }
+            }
+
+            Debug.Log("合計容量 " + SizeSum);
+        }
+
+        public void DrawOnGUI()
+        {
+            /*
+            ProgressBar("TOTAL", SizeSum, 2000f);
+
+            var keys = new string[] { "Textures", "Meshes", "Animations" };
+            // 必要な情報をグラフ化して表示
+            foreach (var k in keys)
+            {
+                ProgressBar(k, datas[k] + 10f, 1000f, 0.5f);
+            }
+            */
+
+            GraphAll(20f, 2000f);
+
+        }
+
+        /*
+        // Custom GUILayout progress bar.
+        void ProgressBar(string label, float value, float max, float scale = 1.0f)
+        {
+            // Get a rect for the progress bar using the same margins as a textfield:
+            Rect rect = GUILayoutUtility.GetRect(18, 16, "TextField");
+            rect.width *= scale;
+            EditorGUI.ProgressBar(rect, value / max, label + " " + value + " KB");
+        }
+        */
+
+
+        Gradient prColor;
+        Gradient PressureColor
+        {
+            get
+            {
+                if (prColor == null)
+                {
+                    var subCol = new Color(0.3f, 0.2f, 0.2f);
+
+                    var cols = new List<GradientColorKey>();
+                    cols.Add(new GradientColorKey(Color.blue + subCol, 0f));
+                    cols.Add(new GradientColorKey(Color.green + subCol, 0.5f));
+                    cols.Add(new GradientColorKey(Color.yellow + subCol, 0.75f));
+                    cols.Add(new GradientColorKey(Color.red + subCol, 1f));
+
+                    prColor = new Gradient();
+                    prColor.colorKeys = cols.ToArray();
+                }
+
+                return prColor;
+            }
+        }
+
+        // 上限サイズ初期値
+        int limitSizeKb = 1000;
+        // 上限サイズ(MB)
+        string LimitSizeMb
+        {
+            get
+            {
+                return (limitSizeKb / 1000f).ToString("0.0") + " MB";
+            }
+        }
+
+        /// <summary>
+        /// アセット容量グラフを表示
+        /// </summary>
+        /// <param name="limitKB"></param>
+        void GraphAll(float startY, float limitKB = 2000f)
+        {
+            // 
+
+            // 上限表示
+            GUI.Label(new Rect(EditorGUIUtility.currentViewWidth - 120, 0, 200, 16), "サイズ上限 " + LimitSizeMb);
+            // 上限設定
+            limitSizeKb = (int)GUI.HorizontalSlider(new Rect(EditorGUIUtility.currentViewWidth - 120, 16, 100, 16), limitSizeKb / 100, 1, 20) * 100;
+
+            // バーオフセット
+            var offsetY = 40f;
+            var stepY = 16f;
+            var barY = 14f;
+            var barStartX = 100;
+
+            // １帯グラフ
+            foreach (var d in datas)
+            {
+
+                //    // カラーとる
+                //     var tmpCol = ViewColors[d.Key];
+
+
+                // バーの長さを求める
+                int barW = (int)(d.Value / limitSizeKb * (EditorGUIUtility.currentViewWidth - 150));
+                // リミットの長さを求める
+                int limitW = (int)((limitSizeKb) / limitSizeKb * (EditorGUIUtility.currentViewWidth - 150));
+                // 割合の算出
+                float rate = (float)barW / (float)limitW;
+                // 色調整
+                var barColor = PressureColor.Evaluate(rate);
+
+
+
+                var backRect = new Rect(barStartX, offsetY, limitW, barY);
+                var labelRect = new Rect(0, offsetY, 100, barY);
+                var sizeRect = new Rect(barStartX, offsetY, 100, barY);
+                var barRect = new Rect(barStartX, offsetY, barW, barY);
+                var limitRect = new Rect(barStartX + limitW, offsetY, 55, barY);
+
+                // 黒下地を描画
+                Handles.DrawSolidRectangleWithOutline(backRect, new Color(0.30f, 0.30f, 0.30f), Color.clear);
+
+                // ラベル
+                GUI.Label(labelRect, d.Key);
+
+                // バーを描画
+                Handles.DrawSolidRectangleWithOutline(barRect, barColor, Color.clear);
+
+                GUI.color = Color.black;
+                // 容量表示
+                GUI.Label(sizeRect, d.Value.ToString("0.0") + " kb");
+                GUI.color = Color.white;
+
+                // リミット表示
+                GUI.Label(limitRect, LimitSizeMb);
+
+                // オフセットずらす
+                offsetY += stepY;
+
+            }
+        }
+
+
+    }
+}
+
+
